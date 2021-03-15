c5251bc6e3b694bd35e6fc5f21d210f780b7fa07
Ambroz <killerbee8@gmail.com>
2021-03-15 14:16:40 +0100

    populacja bazy danych

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,26 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Customer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for ($i=0; $i<50; $i++){
+            $customer = new Customer();
+            $customer->setFirstName($faker->firstName);
+            $customer->setLastName($faker->lastName);
+            $customer->setEmail($faker->email);
+            $customer->setPhoneNumber($faker->phoneNumber);
+
+            $manager->persist($customer);
+        }
 
         $manager->flush();
     }

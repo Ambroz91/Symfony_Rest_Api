7f19d9694283f42341272377e8a6f9758c1b894e
Ambroz <killerbee8@gmail.com>
2021-03-15 17:16:35 +0100

    change

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Customer;
-use App\Repository\CustomerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
